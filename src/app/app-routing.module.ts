897cea1e68e5d59d8ff25df4c52e65f1cee4610d
Wendy Kakuda <wendy.kakuda@diegesis.co.uk>
2020-11-10 15:58:44 +0000

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', redirectTo: '/dashboard', pathMatch: 'full'},{ path: 'heroes', component: HeroesComponent }, {path: 'dashboard', component: DashboardComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
